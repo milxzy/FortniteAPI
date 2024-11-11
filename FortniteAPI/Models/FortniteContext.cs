f6679798aed517832768527519c6eba73234d1c4
Miles <milescollins9@gmail.com>
2024-11-11 16:28:10 -0600

    first commit

---
+++ FortniteAPI/Models/FortniteContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+
+namespace FortniteAPI.Models
+{
+    public class FortniteContext : DbContext
+    {
+        public FortniteContext(DbContextOptions<FortniteContext> options)
+            : base(options) 
+        {
+        }
+        public DbSet<FortnitePlayer> FortnitePlayers { get; set; } = null!;
+    }
+}
---
+++ FortniteAPI/Models/FortnitePlayer.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace FortniteAPI.Models
+{
+    public class FortnitePlayer
+    {
+        public long ID { get; set; }
+        public string? Name { get; set; }
+        public bool IsComplete { get; set; }
+
+    }
+}
